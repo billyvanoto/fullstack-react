480a8d5ed1befeb852d458c31ad45b746d4742a7
billyvanoto <billy.vanoto@gmail.com>
2019-04-17 20:48:14 +0700

    show survey and add survey

---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,43 @@
+import _ from "lodash";
+import React from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router";
+import formFields from "./formFields";
+import * as actions from "../../actions";
+
+const SurveyReview = ({ onCancel, formValues, submitSurvey, history }) => {
+  const reviewFields = _.map(formFields, ({ name, label }) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
+    );
+  });
+  return (
+    <div>
+      <h5>Please Confirm Your Entries</h5>
+      {reviewFields}
+      <button
+        className="yellow darken-3 btn-flat white-text"
+        onClick={onCancel}
+      >
+        Back
+      </button>
+      <button
+        className="green btn-flat right white-text"
+        onClick={() => submitSurvey(formValues, history)}
+      >
+        Send <i className="material-icons right">email</i>
+      </button>
+    </div>
+  );
+};
+
+function mapStateToProps(state) {
+  return { formValues: state.form.SurveyForm.values };
+}
+export default connect(
+  mapStateToProps,
+  actions
+)(withRouter(SurveyReview));
