d28ab7ceafe76af9a9f89bccc576d5b3061012e1
billyvanoto <billy.vanoto@gmail.com>
2019-05-01 13:40:04 +0700

    change office address

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
           <div className="col l12 s12" style={{textAlign:'center'}}>
             <h5 className="white-text">Office</h5>
             <p className="grey-text text-lighten-4">
-              Address : ABC Building, A Street No 1.<br/>
+              Address : DEF Building, A Street No 1.<br/>
               Telp : 01231323
             </p>
           </div>
